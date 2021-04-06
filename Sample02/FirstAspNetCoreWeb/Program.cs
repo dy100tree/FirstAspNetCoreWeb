de2b23d3c9e09c21cc900b81a5d11d84aa1c12ab
dy100tree <44208156@qq.com>
2021-04-06 14:51:05 +0800

    第一次提交项目文件

---
+++ Sample02/FirstAspNetCoreWeb/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace FirstAspNetCoreWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{hostingContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"Content.json", optional: false, reloadOnChange: false)
+                        .AddEnvironmentVariables();
+                })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
